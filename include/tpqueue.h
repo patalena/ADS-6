7c65a1b5656d03d37a278cfc99e4dc6a1488da36
patalena <lenapat0212@gmail.com>
2026-04-25 16:59:41 +0300

    Патанина Елена 25-ИСТ-3

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,61 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <stdexcept>
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    struct Item {
+        T data;
+        Item* next;
+        Item(const T& value) : data(value), next(nullptr) {}
+    };
+    Item* head;
+
+ public:
+    TPQueue() : head(nullptr) {}
+
+    ~TPQueue() {
+        while (head) {
+            Item* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    TPQueue(const TPQueue&) = delete;
+    TPQueue& operator=(const TPQueue&) = delete;
+
+    void push(const T& item) {
+        Item* nnItem = new Item(item);
+        if (head == nullptr) {
+            head = nnItem;
+            return;
+        }
+        if (item.prior > head->data.prior) {
+            nnItem->next = head;
+            head = nnItem;
+            return;
+        }
+        Item* current = head;
+        while (current->next != nullptr && current->next->data.prior >= item.prior) {
+            current = current->next;
+        }
+        nnItem->next = current->next;
+        current->next = nnItem;
+    }
+
+    T pop() {
+        if (head == nullptr) {
+            throw std::out_of_range("TPQueue is empty");
+        }
+        Item* temp = head;
+        T result = head->data;
+        head = head->next;
+        delete temp;
+        return result;
+    }
 };
 
 struct SYM {
